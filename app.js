71fc769d0e0744190ad892e80c5c7f5250c7e336
kylymbai <kylymbaialmazbekuulu@gmail.com>
2022-11-26 00:35:36 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-var red = "STOP!";
+let num = prompt("Оцените наш сервис от 1 до 10");
 
-console.log(red);
-
-var yellow = "WAIT!";
-
-console.log(yellow);
-
-var green = "GO!";
-
-console.log(green);
+if(num < 1){
+    alert("оценка должна быть от 1 до 10")
+}else if(num > 10){
+    alert("оценка должна быть от 1 до 10")
+}else if(num <= 5){
+    alert("мы работаем над улучшением нашего сервиса")
+}else if(num <= 10){
+    alert("Спасибо за высокую оценку!")
+}else{
+    alert("ERROR! Write a number!")
+}
